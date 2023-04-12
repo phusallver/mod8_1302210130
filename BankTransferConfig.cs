c5ff79991fb199a2e49cd34efd9b6238f1306460
phusallver <103794258+phusallver@users.noreply.github.com>
2023-04-12 17:11:22 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace mod8_1302210130
+{
+    internal class BankTransferConfig
+    {
+        public Konfig konfig;
+        private const string filepath = @"configidk.json";
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                ReadKonfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteKonfigFile();
+            }
+        }
+        public void ReadKonfigFile()
+        {
+            string hasilBaca = File.ReadAllText(filepath);
+            konfig = JsonSerializer.Deserialize<Konfig>(hasilBaca);
+        }
+        public void WriteKonfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string tulisan = JsonSerializer.Serialize(konfig, options);
+            File.WriteAllText(filepath, tulisan);
+        }
+        public void SetDefault()
+        {
+            konfig = new Konfig();
+            konfig.lang = "id";
+            konfig.transfer = new Transfer();
+            konfig.transfer.threshold = 25000000;
+            konfig.transfer.low_fee = 6500;
+            konfig.transfer.high_fee = 15000;
+            konfig.methods = new List<string>();
+            konfig.methods.Add("RTO(real-time)");
+            konfig.methods.Add("SKN");
+            konfig.methods.Add("RTGS");
+            konfig.methods.Add("BI FAST");
+            konfig.conformation = new Conformation(); 
+            konfig.conformation.en = "yes";
+            konfig.conformation.id = "ya";
+        }
+    }
+}
+
+public class Konfig
+{
+    public string lang { get; set; }
+    public Transfer transfer { get; set; }
+    public List<string> methods { get; set; }
+    public Conformation conformation { get; set; }
+    public Konfig() { }
+}
+public class Transfer
+{
+    public int threshold { get; set; }
+    public int low_fee { get; set; }
+    public int high_fee { get; set;}
+    public Transfer() { }
+}
+public class Conformation
+{
+    public string en { get; set; }
+    public string id { get; set; }
+    public Conformation() { }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using mod8_1302210130;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BankTransferConfig conf = new BankTransferConfig();
+        int transferan; int ngasal; string confirm;
+        if (conf.konfig.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+            transferan = Convert.ToInt32(Console.ReadLine());
+            if (transferan > conf.konfig.transfer.threshold)
+            {
+                Console.WriteLine("Transfer fee = " + conf.konfig.transfer.high_fee);
+                Console.WriteLine("Total amount = " + (transferan + conf.konfig.transfer.high_fee));
+            }
+            else
+            {
+                Console.WriteLine("Transfer fee = " + conf.konfig.transfer.low_fee);
+                Console.WriteLine("Total amount = " + (transferan + conf.konfig.transfer.low_fee));
+            }
+            Console.WriteLine("Select transfer meethod:");
+            for (int i = 0; i < conf.konfig.methods.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + conf.konfig.methods[i]);
+            }
+            ngasal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type "+conf.konfig.conformation.en+" to confirm the trasaction:");
+            confirm = Console.ReadLine();
+            if (confirm == conf.konfig.conformation.en)
+            {
+                Console.WriteLine("Transfer is completed");
+            }
+            else
+            {
+                Console.WriteLine("Transfer is canceled");
+            }
+        }
+        else if (conf.konfig.lang == "id")
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan anda transfer: ");
+            transferan = Convert.ToInt32(Console.ReadLine());
+            if (transferan > conf.konfig.transfer.threshold)
+            {
+                Console.WriteLine("Biaya transfer = " + conf.konfig.transfer.high_fee);
+                Console.WriteLine("Total biaya = " + (transferan + conf.konfig.transfer.high_fee));
+            }
+            else
+            {
+                Console.WriteLine("Biaya transfer = " + conf.konfig.transfer.low_fee);
+                Console.WriteLine("Total biaya = " + (transferan + conf.konfig.transfer.low_fee));
+            }
+            Console.WriteLine("Pilih metode transfer:");
+            for (int i = 0; i < conf.konfig.methods.Count; i++)
+            {
+                Console.WriteLine((i + 1) +". "+ conf.konfig.methods[i]);
+            }
+            ngasal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type " + conf.konfig.conformation.id + " to confirm the trasaction:");
+            confirm = Console.ReadLine();
+            if (confirm == conf.konfig.conformation.id)
+            {
+                Console.WriteLine("Proses transfer berhasil");
+            }
+            else
+            {
+                Console.WriteLine("Transfer dibatalkan");
+            }
+        }
+
+    }
+}
